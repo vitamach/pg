b223348d79951a29c6e2e6f292133c2d5162a166
vitamach <nablbostiahry@gmail.com>
2024-10-03 08:57:04 +0000

    drobna uprava

--- first.py
+++ first.py
@@ -1,4 +1,13 @@
-def hello():
-    print("Hello world!")
+def sudy_nebo_lichy(cislo):
+ 
+    if cislo % 2 == 0:
+        print(f"Číslo {cislo} je sudé")
+ 
+    else:
+        print(f"Číslo {cislo} je liché")
 
-hello()
+
+sudy_nebo_lichy(6)
+
+sudy_nebo_lichy(7)
+
---
+++ five_stars.py
@@ -0,0 +1,11 @@
+
+
+def in_range(min_number, max_number, number):
+    if (number > min_number) and (number < max_number): #porovnavani cisla zadaneho
+        print("Is in range")
+
+    else:
+        print("is not in range")
+
+
+in_range(100,1000, 455)
