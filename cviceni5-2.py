1e8ae8cfece857faf3ff0bbed04bc2391b5d924d
vitamach <nablbostiahry@gmail.com>
2024-10-31 09:49:51 +0000

    zmena

--- cviceni5-2.py
+++ cviceni5-2.py
@@ -1,36 +1,20 @@
-import sys
+import sys 
 import csv
 
 def nacti_csv(soubor):
-    seznam = []
-    try:
-        with open(soubor, "r") as file:
-            reader = cvs.reader(file)
-            for radek in reader:
-                seznam.append(radek)
-        
-    except FileNotFoundError:
-        print(f"Soubor {soubor} nebyl nalezen.")
-        return[]
+    data = []
+    with open(soubor, "r") as fp:
+        reader = csv.reader(fp)
+        for row in reader:
+            data.append(row)
+    return data 
 
-    except FileExistsError:
-        print(f"Soubor neextuje")
-        return []
-
-    except IOError:
-        print(f"Došlo k chybě při čtení souboru: {soubor}")
-        return []
-    return seznam
-
-def spoj_data(data1, data2):
-     excel.cvs + excel2.cvs
-
-if __name__ == "__main__": 
+if __name__ == "__main__":
     try:
         soubor1 = sys.argv[1]
         soubor2 = sys.argv[2]
         csv_data1 = nacti_csv(soubor1)
         csv_data2 = nacti_csv(soubor2)
-        print(csv_data1, csv_data2)
+
     except Exception:
-        print("Neni chyba")
+        
