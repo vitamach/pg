c9301a83326ae105f8942773dbd562f69413f68c
vitamach <nablbostiahry@gmail.com>
2024-11-03 10:10:22 +0000

    uzmaka

--- fifth.py
+++ fifth.py
@@ -1,34 +1,50 @@
 import sys
 
-class ChybaNeniObrazek(Exception):
-    pass
+jpeg_header = b'\xff\xd8'
+gif_header1 = b'GIF87a'
+gif_header2 = b'GIF89a'
+png_header = b'\x89PNG\r\n\x1a\n'
+
 
 def read_header(file_name, header_length):
     try:
         with open(file_name, "rb") as otevreny_soubor:
             return otevreny_soubor.read(header_length)
-        
-    except(FileNotFoundError, IOError):
-        raise ChybaNeniObrazek("Nelze načíst soubor nebo neexistuje")
+    except FileNotFoundError:
+        raise Exception(f"Soubor '{file_name}' nebyl nalezen.")
 
-def detect_format(header):
-    if header.startswith(b'\xff\xd8') and header.endswith(b'\xff\xd9'):
-        return "JPEG"
-    elif header.startswith(b'GIF87a') or header.startswith(b'GIF89a'):
-        return "GIF"
-    elif header.startswith(b'\x89PNG\r\n\x1a\n'):
-        return "PNG"
-    return "Neznámý formát"
 
-if __name__ == "__main__":
-    try: 
+def is_jpeg(file_name):
+    header = read_header(file_name, len(jpeg_header))
+    return header.startswith(jpeg_header)
+
+
+def is_gif(file_name):
+    header = read_header(file_name, len(gif_header1))
+    return header.startswith(gif_header1) or header.startswith(gif_header2)
+
+
+def is_png(file_name):
+    header = read_header(file_name, len(png_header))
+    return header.startswith(png_header)
+
+
+def print_file_type(file_name):
+    if is_jpeg(file_name):
+        print(f'Soubor {file_name} je typu jpeg')
+    elif is_gif(file_name):
+        print(f'Soubor {file_name} je typu gif')
+    elif is_png(file_name):
+        print(f'Soubor {file_name} je typu png')
+    else:
+        print(f'Soubor {file_name} je neznámého typu')
+
+
+if __name__ == '__main__':
+    try:
         file_name = sys.argv[1]
-        header = read_header(file_name)
-
-        format = detect_format
-
+        print_file_type(file_name)
     except IndexError:
-        raise ValueError("Zadej název souboru jako argument")
-    
-    except ChybaNeniObrazek as e:
-        raise ChybaNeniObrazek(f"Chyba: {e}")
+        print("Chyba: Zadej název souboru jako argument.")
+    except Exception as e:
+        print(f"Chyba: {e}")
